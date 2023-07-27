6e9b389851b34014394c7d2647758caf01584821
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-07-27 13:26:18 +0200

    🚀 Deploy Bootstrap

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class HomeController extends AbstractController
+{
+    #[Route("/", name: "home")]
+    public function index(AuthenticationUtils $authenticationUtils): Response
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+        
+        return $this->render("pages/index.html.twig", [
+            "controller_name" => "LoginController",
+            "last_username" => $lastUsername,
+            "error" => $error,
+        ]);
+    }
+}
