ed329832c52fd012abd6623922f34e8a7139158f
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-08-01 18:19:37 +0200

    🚧 Creation of the pages

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
@@ -25,15 +26,17 @@ class UserController extends AbstractController
 
 
     #[Route("/users/create", name: "users_create")]
-    public function new(EntityManagerInterface $entityManager, Request $request): Response
+    public function new(EntityManagerInterface $entityManager, Request $request, UserPasswordHasherInterface $userPasswordHasher): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $password = $userPasswordHasher->hashPassword($user, $form->get("password")->getData());
             $currentDate = \DateTimeImmutable::createFromFormat("Y-m-d H:i:s", date("Y-m-d H:i:s"));
 
+            $user->setPassword($password);
             $user->setCreatedAt($currentDate);
             $user->setUpdatedAt($currentDate);
 
@@ -42,7 +45,7 @@ class UserController extends AbstractController
 
             $this->addFlash("success", "L'utilisateur a bien été ajouté.");
 
-            return $this->redirectToRoute("users_list");
+            return $this->redirectToRoute("home");
         }
 
         return $this->render("pages/users/create.html.twig", [
