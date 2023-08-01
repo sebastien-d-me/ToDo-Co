6d03bf20a29ae1921c3e88cd00536808d452b953
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-08-01 14:56:14 +0200

    🚧 Creation of the pages

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,23 +5,65 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder
-            ->add('email')
-            ->add('password')
-            ->add('username')
-        ;
+        $builder->add("username", TextType::class, [
+            "attr" => ["class" => "form-control"],
+            "label" => "Nom d'utilisateur", 
+            "label_attr" => ["class" => "form-label fw-bold"],
+            "required" => true,   
+        ]);
+
+        $builder->add("password", RepeatedType::class, [
+            "first_options" => [
+                "label" => "Mot de passe", 
+                "label_attr" => [
+                    "class" => "form-label fw-bold"
+                ],
+                "row_attr" => ["class" => "mt-2"]
+            ],
+            "invalid_message" => "Les mots de passes ne correspondent pas.",
+            "options" => [
+                "attr" => ["class" => "form-control"]
+            ],
+            "required" => true,
+            "second_options" => [
+                "label" => "Tapez le mot de passe à nouveau", 
+                "label_attr" => ["class" => "form-label fw-bold"],
+                "row_attr" => ["class" => "mt-2"]
+            ],
+            "type" => PasswordType::class,
+        ]);
+
+        $builder->add("email", EmailType::class, [
+            "attr" => ["class" => "form-control"],
+            "label" => "Adresse email", 
+            "label_attr" => ["class" => "form-label fw-bold"],
+            "required" => true,
+            "row_attr" => ["class" => "mt-2"]
+        ]);
+
+        $builder->add("save", SubmitType::class, [
+            "attr" => ["class" => "btn btn-success"],
+            "label" => "Ajouter",
+            "row_attr" => ["class" => "d-flex justify-content-end mt-3"],
+        ]);
     }
 
+    
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => User::class,
+            "data_class" => User::class,
         ]);
     }
 }
