18e41c92ebf0428713f0e2cbea5e8474b3571b9b
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-08-16 14:57:17 +0200

    🚧 Creation of the test

---
+++ tests/Form/TaskTypeTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Form\TaskType;
+use App\Entity\Task;
+use App\Repository\UserRepository;
+use Symfony\Component\Form\Test\TypeTestCase;
+
+class TaskTypeTest extends TypeTestCase
+{
+    public function testTaskType(): void
+    {   
+        $formData = [
+            "title" => "Nunc gravida ligula non est convallis faucibus",
+            "content" => "Morbi sit amet molestie sem. Quisque mattis posuere neque ullamcorper pulvinar.",
+        ];
+
+        $task = new Task();
+        $form = $this->factory->create(TaskType::class, $task);
+
+        $expected = new Task();
+        $expected->setTitle($formData["title"]);
+        $expected->setContent($formData["content"]);
+        $form->submit($formData);
+
+        $this->assertTrue($form->isSynchronized());
+        $this->assertEquals($expected, $task);
+    }
+}
