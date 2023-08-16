b16395ebb117721dd988a047e5790fc2991d8d0d
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-08-16 20:14:09 +0200

    🚧 Creation of the test

--- tests/Controller/UserControllerTest.php
+++ tests/Controller/UserControllerTest.php
@@ -18,4 +18,21 @@ class UserControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
     }
+
+    public function testUserCreate(): void 
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = $userRepository->findOneByEmail("admin@test.com");
+        $client->loginUser($user);
+
+        $crawler = $client->request("GET", "/users/create");
+
+        $form = $crawler->selectButton("Sauvegarder")->form();
+        $form["user[username]"] = "john.doe";
+        $client->submit($form);
+        
+        $this->assertSelectorTextContains("div.alert.alert-success", "L'utilisateur a bien été ajouté.");
+    }
 }
