278d63a8f5d362ff14fe9f93cbecae0d35cbff9f
Sébastien D <sebastien.delahaye.contact@gmail.com>
2023-08-17 23:59:44 +0200

    🚧 Creation of the test

--- tests/Controller/TaskControllerTest.php
+++ tests/Controller/TaskControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use App\Repository\TaskRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
@@ -29,5 +30,24 @@ class TaskControllerTest extends WebTestCase
         $crawler = $client->request("GET", "/tasks/done");
 
         $this->assertResponseIsSuccessful();
+    }
+
+    public function testTaskDelete(): void
+    {
+        $client = static::createClient();
+        $usersRepository = static::getContainer()->get(UserRepository::class);
+        $user = $usersRepository->findOneByEmail("admin@test.com");
+        $client->loginUser($user);
+
+        $tasksRepository = static::getContainer()->get(TaskRepository::class);
+        $exempleTask = $tasksRepository->findAll()[0];
+        $exempleTask->isIsDone() ? $exempleTaskStatut = "completed" : $exempleTaskStatut = "uncompleted";
+
+        $crawler = $client->request("GET", "/tasks/".$exempleTask->getId()."/delete/".$exempleTaskStatut);
+        $client->followRedirects();
+
+        $checkExempleTask = $tasksRepository->findAll()[0];
+
+        $this->assertEquals($exempleTask, $checkExempleTask);
     }
 }
